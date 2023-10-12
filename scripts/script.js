edce5d81e77feb5f4a23e7a39ac6e0871818897a
DavidBakalov21 <davidbakalov33@gmail.com>
2023-10-13 00:09:15 +0300

    hints

--- scripts/script.js
+++ scripts/script.js
@@ -1,73 +1,92 @@
 function Submit() {
-  let fields = {
-    Name: document.getElementById("Name").value,
-    Age: document.getElementById("Age").value,
-    Password: document.getElementById("Password").value,
-    PasswordConfirm: document.getElementById("PasswordConfirm").value,
-    Email: document.getElementById("Email").value,
-    BirthDate: document.getElementById("BirthDate").value,
-  };
-
+  let fieldIds = [
+    "Name",
+    "Age",
+    "Password",
+    "PasswordConfirm",
+    "Email",
+    "BirthDate",
+  ];
+  let Normal = true;
   let ITBA = document.getElementById("ITBA");
   let BE = document.getElementById("BE");
   let BDE = document.getElementById("BDE");
   let successValue = document.getElementById("inputRange").value;
-  //console.log(document.getElementById(''))
   let text = "";
 
-  if (ITBA.checked == true) {
+  if (ITBA.checked) {
     text = "ITBA";
   }
-  if (BE.checked == true) {
+  if (BE.checked) {
     text = "BE";
   }
-  if (BDE.checked == true) {
+  if (BDE.checked) {
     text = "BDE";
   }
-
+  if (!BDE.checked && !BE.checked && !ITBA.checked) {
+    Normal = false;
+    document.getElementById("hintRad").style.display = "block";
+  } else {
+    Normal = true;
+    document.getElementById("hintRad").style.display = "none";
+  }
   let FinalText = document.getElementById("finalText");
-  let EmailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.com$/;
+  let EmailPattern = /^[a-zA-Z0-9._-]+@gmail+\.com$/;
+
   let PasswordPattern =
     /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{12,}$/;
-  let Normal = true;
-  for (let id in fields) {
-    if (fields[id] == "") {
+  for (let fieldId of fieldIds) {
+    let hintElement = document.getElementById("hint" + fieldId);
+    if (hintElement) {
+      hintElement.style.display = "none";
+    }
+  }
+  let hintPassword = document.getElementById("hintPassword");
+  let hintEmail = document.getElementById("hintEmail");
+
+  for (let fieldId of fieldIds) {
+    let element = document.getElementById(fieldId);
+    let hintElement = document.getElementById("hint" + fieldId);
+    if (element && hintElement && element.value === "") {
       Normal = false;
-      alert("Error is in " + id + ", it is empty");
+      hintElement.style.display = "block";
     }
   }
 
-  if (!EmailPattern.test(fields["Email"])) {
+  let emailElement = document.getElementById("Email");
+  if (!EmailPattern.test(emailElement.value)) {
     Normal = false;
-    alert("Error is in " + fields["Email"] + ", it is written in a wrong way");
+    hintEmail.style.display = "block";
+  }
+  let passwordConfirmElement = document.getElementById("PasswordConfirm");
+  let passwordElement = document.getElementById("Password");
+  if (!PasswordPattern.test(passwordElement.value)) {
+    Normal = false;
+    hintPassword.style.display = "block";
   }
 
-  if (!PasswordPattern.test(fields["Password"])) {
+  if (passwordElement.value != passwordConfirmElement.value) {
     Normal = false;
-    alert(
-      "Error is in " + fields["Password"] + ", it is written in a wrong way"
-    );
+    hintPassword.style.display = "block";
   }
-  if (fields["Password"] != fields["PasswordConfirm"]) {
-    Normal = false;
-    alert(
-      "Error is in " + fields["Password"] + ", it is not like Password confirm"
-    );
-  }
-
   if (Normal == true) {
+    let nameValue = document.getElementById("Name").value;
+    let ageValue = document.getElementById("Age").value;
+    let passwordValue = passwordElement.value;
+    let emailValue = emailElement.value;
+    let birthDateValue = document.getElementById("BirthDate").value;
     FinalText.textContent =
       "I am " +
-      fields["Name"] +
+      nameValue +
       ", I am " +
-      fields["Age"] +
+      ageValue +
       " years old" +
       ", Password is " +
-      fields["Password"] +
+      passwordValue +
       ", Email is " +
-      fields["Email"] +
+      emailValue +
       ", BDate is " +
-      fields["BirthDate"] +
+      birthDateValue +
       ", Your program is:" +
       text +
       ", Your success is:" +
